33636c88098d43745926fc0328d7da46bef048e0
AdithyaS25 <adithyahejamady@gmail.com>
2026-03-06 12:22:32 +0530

    fix: correct CI workflow - resolved parsing error

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,5 +2,5 @@ import '@testing-library/jest-dom';
 
 // Polyfill TextEncoder/TextDecoder for react-router in jsdom
 const { TextEncoder, TextDecoder } = require('util');
-(globalThis as any).TextEncoder = TextEncoder;
-(globalThis as any).TextDecoder = TextDecoder;
+(globalThis.TextEncoder = TextEncoder); 
+(globalThis.TextDecoder = TextDecoder);
